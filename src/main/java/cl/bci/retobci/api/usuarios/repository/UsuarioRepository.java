54cffa30c16b257f62313f379ce8073990b43f35
RodrigoMoralesQuiroz <rmquiroz@gmail.com>
2026-03-01 11:59:34 -0300

    Reto ApiUsuariosJava

--- src/main/java/cl/bci/retobci/api/usuarios/repository/UsuarioRepository.java
+++ src/main/java/cl/bci/retobci/api/usuarios/repository/UsuarioRepository.java
@@ -6,9 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
     @Override
     Optional<Usuario> findById(Long aLong);
     Optional<Usuario> findByEmail(String email);
-
 }
+
+
